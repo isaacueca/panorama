74b8076e7ccde61a047a2677e820b8bee1a0248d
Scotty Allen <scotty@ooyala.com>
2010-08-20 23:43:17 -0700

    App skeleton.

---
+++ ipad/Panorama/Classes/PanoramaAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  PanoramaAppDelegate.h
+//  Panorama
+//
+//  Created by Scotty Allen on 8/20/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PanoramaViewController;
+
+@interface PanoramaAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    PanoramaViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet PanoramaViewController *viewController;
+
+@end
+
---
+++ ipad/Panorama/Classes/PanoramaViewController.h
@@ -0,0 +1,16 @@
+//
+//  PanoramaViewController.h
+//  Panorama
+//
+//  Created by Scotty Allen on 8/20/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanoramaViewController : UIViewController {
+
+}
+
+@end
+
