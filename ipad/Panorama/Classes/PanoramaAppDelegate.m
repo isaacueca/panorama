a2bf7d933199533a98a6b7f610dc16956a2c5675
Scotty Allen <scotty@ooyala.com>
2010-08-22 11:16:58 -0700

    Added screen mirroring for the demo.

--- ipad/Panorama/Classes/PanoramaAppDelegate.m
+++ ipad/Panorama/Classes/PanoramaAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "PanoramaAppDelegate.h"
 #import "PanoramaViewController.h"
+#import "UIApplication+ScreenMirroring.h"
 
 @implementation PanoramaAppDelegate
 
@@ -19,37 +20,12 @@
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
-    
-    // Override point for customization after app launch. 
-    [window addSubview:viewController.view];
-    [window makeKeyAndVisible];
+  [window addSubview:viewController.view];
+  [window makeKeyAndVisible];
+  [[UIApplication sharedApplication] setupScreenMirroring];
 
-	return YES;
+  return YES;
 }
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    /*
-     Called when the application is about to terminate.
-     See also applicationDidEnterBackground:.
-     */
-}
-
 
 #pragma mark -
 #pragma mark Memory management
@@ -60,12 +36,10 @@
      */
 }
 
-
 - (void)dealloc {
     [viewController release];
     [window release];
     [super dealloc];
 }
-
 
 @end
